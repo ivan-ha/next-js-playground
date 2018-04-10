0a2e8ae1f73fb4e93a658722b8fee69618589715
Ivan HA <hangolam@gmail.com>
2018-04-10 23:59:49 +0800

    feat: fetch data in index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import fetch from 'isomorphic-unfetch'
+
 import Layout from '../components/layout'
 import PostLink from '../components/postLink'
 import { blogList } from '../constants/blogList'
 
-const Index = () => (
+const Index = props => (
   <Layout>
     <p>Welcome to next.js</p>
     <h1>A Blog</h1>
@@ -15,7 +17,22 @@ const Index = () => (
         />
       ))}
     </ul>
+
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(({ show }) => (
+        <PostLink id={show.id} key={show.id} title={show.name} />
+      ))}
+    </ul>
   </Layout>
 )
+
+Index.getInitialProps = async () => {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+  return {
+    shows: data,
+  }
+}
 
 export default Index
